88db372b9a1ee3a2814f42b233a7fbbedf9ce403
Divyasree Kolli <kollidivya94@gmail.com>
2026-02-08 12:01:00 +0530

    Initial Fast API structure and Health check

--- app/main.py
+++ app/main.py
@@ -3,5 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def health_check():
+def main_page():
     return "Welcome to Skill Progression & Readiness"
+
+@app.get("/health")
+def health_check():
+    return "OK"
